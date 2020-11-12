0e512eb167271683b5d87de7519bd0c5e73988fa
Andrey0295 <andreevicandrej00@gmail.com>
2020-11-12 13:57:12 +0200

    Выполняет третье задание

---
+++ src/js/super-timer.js
@@ -0,0 +1,70 @@
+
+
+const refs = {
+    days: document.querySelector('[data-value="days"]'),
+    hours: document.querySelector('[data-value="hours"]'),
+    mins: document.querySelector('[data-value="mins"]'),
+    secs: document.querySelector('[data-value="secs"]'),
+}
+
+
+class CountdownTimer {
+    constructor({selector, targetDate}){
+        this.selector = selector;
+        this.targetDate = targetDate;
+    }
+}
+
+
+const timer = {
+    start(){
+        const startTime = new CountdownTimer({
+            selector: '#timer-1',
+            targetDate: new Date('Jan 15, 2021'),
+
+        });
+
+    
+        setInterval(() => {
+            const currentTime = Date.now();
+            const deltaTime = startTime.targetDate - currentTime;
+            const {days, hours, mins, secs} = getTimeComponents(deltaTime);
+            console.log(`${days}:${hours}:${mins}:${secs}`);
+           updateClockface({days, hours, mins, secs} )
+            
+
+        }, 1000);
+    },
+}
+timer.start();
+
+function updateClockface({days, hours, mins, secs}){
+    
+    refs.days.textContent = `${days}`
+    refs.hours.textContent = `${hours}`
+    refs.mins.textContent = `${mins}`
+    refs.secs.textContent = `${secs}`
+}
+
+new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Jul 17, 2019'),
+  });
+
+
+
+function pad(value){
+    return String(value).padStart(2, '0');
+}
+
+
+function getTimeComponents(time){
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins =pad( Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs =pad( Math.floor((time % (1000 * 60)) / 1000));
+    return {days, hours, mins, secs};
+}
+
+
+console.log('Hello')
